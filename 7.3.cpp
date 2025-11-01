68c529fb94a1c52da74dedfbb47a6a93026ffecc
Code0rbit <saksha2006@gmail.com>
2025-11-01 19:39:28 +0530

    7.3.cpp

--- 7.3.cpp
+++ 7.3.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+class Vehicle {
+public:
+    Vehicle() {
+        cout << "This is a Vehicle\n";
+    }
+};
+
+class Fare {
+public:
+    Fare() {
+        cout << "Fare of Vehicle\n";
+    }
+};
+
+class Car : public Vehicle {
+public:
+    Car() {
+        cout << "This Vehicle is a Car\n";
+    }
+};
+
+class Bus : public Vehicle, public Fare {
+public:
+    Bus() {
+        cout << "The Vehicle is a Bus with Fare\n";
+    }
+};
+
+int main() {
+    Bus obj2;
+    return 0;
+}
